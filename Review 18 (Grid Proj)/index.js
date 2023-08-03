9f10e934c12cffcef572fcb07f4e8a2d649ad38c
Rogine <111555650+rogineceniza@users.noreply.github.com>
2023-08-03 15:34:51 +0800

    Add files via upload

---
+++ Review 18 (Grid Proj)/index.js
@@ -0,0 +1,25 @@
+// const btn = document.querySelector("#btn");
+
+// btn.addEventListener("click", () => {
+//     alert("Helloooooooooooooooooooooooo");
+// });
+
+// const btn = document.querySelector("#btn");
+
+// btn.addEventListener("click" ,() =>{
+// btn.classList.toggle("purple");
+// });
+
+
+const hamburger = document.querySelector(".hamburger");
+const bar1 = document.querySelector(".bar1");
+const bar2 = document.querySelector(".bar2");
+const bar3 = document.querySelector(".bar3");
+const mobileNav = document.querySelector(".mobileNav");
+
+hamburger.addEventListener("click", () =>{
+    bar1.classList.toggle("animateBar1");
+    bar2.classList.toggle("animateBar2");
+    bar3.classList.toggle("animateBar3");
+    mobileNav.classList.toggle("openDrawer");
+});
